3b0884f41b7899fac881f7c9225b83f44479db18
jason.sh <jasonhensley93@gmail.com>
2019-12-16 11:55:43 -0600

    Added more information...

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-
-## run speed test, post results in Mbps, change url if in another state.
-SPEED=$(curl -m 30 -w '%{speed_download}\n' -o /dev/null -s  https://tx-us-ping.vultr.com/vultr.com.100MB.bin) ; BSPEED=$(expr $(expr $(echo $SPEED | cut -d'.' -f1) / 1024 / 1024) \* 8) ; echo $BSPEED" Mbps"
+ #!/bin/bash
+ ## run speed test, post results in Mbps, change url if in another state.
+ SPEED=$(curl -m 30 -w '%{speed_download}\n' -o /dev/null -s  https://tx-us-ping.vultr.com/vultr.com.100MB.bin); BSPEED=$(expr $(expr $(echo $SPEED | cut -d'.' -f1) / 1024 / 1024) \* 8); date && echo "Download: $BSPEED Mbps"
